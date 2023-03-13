5f9be855e6e9147ee5fb9c878bdf2f613ba11674
Nikita <nilkanthnikita24@gmail.com>
2023-03-13 15:47:22 +0530

    NewClass

---
+++ src/main/java/com/example/Nykaams/Haircare.java
@@ -0,0 +1,13 @@
+package com.example.Nykaams;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class Haircare {
+    @GetMapping("/Shampoos")
+    public String getData(){
+        return  "buy one get on free";
+    }
+}

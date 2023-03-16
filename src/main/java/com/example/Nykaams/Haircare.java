67fa9d69a84688e411f5e1307335b6259ef88a89
Nikita <nilkanthnikita24@gmail.com>
2023-03-16 17:34:14 +0530

    NewBranch

--- src/main/java/com/example/Nykaams/Haircare.java
+++ src/main/java/com/example/Nykaams/Haircare.java
@@ -3,6 +3,10 @@ package com.example.Nykaams;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @RestController
 public class Haircare {
